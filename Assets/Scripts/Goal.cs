3cff567509550a0de0f79b0108ea0389d108e7cc
Satvik Nagpal <satviknagpal2401@gmail.com>
2024-07-21 18:48:25 +0530

    Scene index added

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -2,11 +2,20 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    [SerializeField] bool m_LoadNextScene;
+    [SerializeField] int m_SceneIndexToLoad;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Debug.Log("You win!");
+            if (m_LoadNextScene)
+            {
+                SceneLoadingManager.Instance.LoadNextScene();
+            }
+            else
+            {
+                SceneLoadingManager.Instance.LoadScene(m_SceneIndexToLoad);
+            }
         }
     }
 }
